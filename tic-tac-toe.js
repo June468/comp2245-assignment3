821874b04449c0f5d6d143d603fb6a24573cc22a
June Douglas <june.douglas@my.fiveislands.uwi.edu>
2023-10-18 10:15:09 -0400

    Added square

---
+++ tic-tac-toe.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const squares = document.querySelectorAll('board');
+    squares.forEach(square => square.classList.add('square'));
+});
