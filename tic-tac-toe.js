9b245a1e64073b483fd1e72e2be36ddae0b394fa
June Douglas <june.douglas@my.fiveislands.uwi.edu>
2023-10-18 22:54:55 -0400

    exercise four

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -4,17 +4,39 @@ document.addEventListener('DOMContentLoaded', function () {
     const playerX = 'X';
     const playerO = 'O';
     let xTurn = true;
+    const status = document.getElementById('status');
+
+    const wins = [
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // columns
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
+        [0, 4, 8], [2, 4, 6] // diagonals
+    ];
+
+    function checkForWin(symbol) {
+        return wins.some(combo => {
+            return combo.every(index => squares[index].classList.contains(symbol));
+        });
+    }
 
     squares.forEach(square => {
         square.classList.add('square');
+
         square.addEventListener('click', function () {
             if (!square.classList.contains(playerX) && !square.classList.contains(playerO)) {
                 if (xTurn) {
                     square.classList.add(playerX);
                     square.textContent = 'X';
+                    if (checkForWin(playerX)) {
+                        status.textContent = "Congratulations! X is the Winner!";
+                        status.classList.add('you-won');
+                    }
                 } else {
                     square.classList.add(playerO);
                     square.textContent = 'O';
+                    if (checkForWin(playerO)) {
+                        status.textContent = "Congratulations! O is the Winner!";
+                        status.classList.add('you-won');
+                    }
                 }
                 xTurn = !xTurn;
             }
