959a446809145c4afa0fb51cdbdd11a2a39409bf
June Douglas <june.douglas@my.fiveislands.uwi.edu>
2023-10-18 22:46:23 -0400

    exercise two

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,4 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const squares = document.querySelectorAll('board');
+    const squares = document.querySelectorAll('#board div');
     squares.forEach(square => square.classList.add('square'));
+    const playerX = 'X';
+    const playerO = 'O';
+    let xTurn = true;
+
+    squares.forEach(square => {
+        square.classList.add('square');
+        square.addEventListener('click', function () {
+            if (!square.classList.contains(playerX) && !square.classList.contains(playerO)) {
+                if (xTurn) {
+                    square.classList.add(playerX);
+                    square.textContent = 'X';
+                } else {
+                    square.classList.add(playerO);
+                    square.textContent = 'O';
+                }
+                xTurn = !xTurn;
+            }
+        });
+    });
 });
